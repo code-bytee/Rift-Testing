ddf52fb7423c1a9f2b2d11633befd543b11e9e93
code-bytee <bekketristan@gmail.com>
2025-04-26 11:28:02 +0200

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,220 @@
+
+repeat task.wait() until game:IsLoaded()
+
+local LibraryRepository = "https://raw.githubusercontent.com/synnyyy/Obsidian/refs/heads/main"
+local Libraries = {
+    Library = LibraryRepository .. "/Library.lua",
+    Information = "https://raw.githubusercontent.com/Synergy-Networks/products/refs/heads/main/Rift/Assets/Information.lua",
+    API = "https://sdkapi-public.luarmor.net/library.lua"
+}
+local KeyValidated = true
+
+local Total = 3
+local Completed = 0
+
+for Name, Url in next, Libraries do
+    task.spawn(function()
+        local Content = game:HttpGet(Url)
+        Libraries[Name] = loadstring(Content)()
+        Completed = Completed + 1
+    end)
+end
+
+repeat
+    task.wait()
+until Completed == Total
+
+
+if game.PlaceId == 16732694052 then
+    Libraries.API.script_id = "9def910837a9eb687d5d76eabec435a5"
+elseif game.PlaceId == 72907489978215 then
+    Libraries.API.script_id = "1c57708a6733fcdac89be981d028aebc"
+elseif game.PlaceId == 18687417158 then
+    Libraries.API.script_id = "296d23036fbb1af463d3ad03f08a67a4"
+elseif game.PlaceId == 70876832253163 then
+     Libraries.API.script_id = "373f5d42922fa6b5ac57adbb41b8015f"
+elseif game.PlaceId == 116495829188952 then
+    return Libraries.Library:Notify({
+        Title = string.format('<font color="rgb(%d, %d, %d)">◆ Notice</font>', 
+            Libraries.Library.Scheme.AccentColor.R * 255, 
+            Libraries.Library.Scheme.AccentColor.G * 255, 
+            Libraries.Library.Scheme.AccentColor.B * 255
+        ),
+        Description = "Please execute Rift when you are in a ACTUAL game, not the lobby.",
+        Time = 5,
+    }) 
+else
+    return Libraries.Library:Notify({
+        Title = string.format('<font color="rgb(%d, %d, %d)">◆ Execution Error</font>', 
+            Libraries.Library.Scheme.AccentColor.R * 255, 
+            Libraries.Library.Scheme.AccentColor.G * 255, 
+            Libraries.Library.Scheme.AccentColor.B * 255
+        ),
+        Description = "Rift does not support this game.\n\nIf you feel that this is a mistake, please contact us on vaultcord.win/synergy.",
+        Time = 5,
+    }) 
+end
+
+if getfenv().script_key then
+    KeyValidated = true
+elseif isfile("RiftAssets/SavedKey.txt") then
+    local HasValidSavedKey = Libraries.API.check_key(readfile("RiftAssets/SavedKey.txt"))
+    if HasValidSavedKey.code == "KEY_VALID" then
+        KeyValidated = true
+        getfenv().script_key = readfile("RiftAssets/SavedKey.txt")
+    else
+        delfile("RiftAssets/SavedKey.txt")
+    end
+end
+
+if not KeyValidated then
+    local Window = Libraries.Library:CreateWindow({
+        Title = "Rift",
+        Icon = getcustomasset("RiftAssets/Logo.png"),
+        Footer = "Key System · " .. Libraries.Information.VERSION .. " · vaultcord.win/synergy",
+        NotifySide = "Left",
+        ShowCustomCursor = true,
+        ForceCheckbox = false,
+        NotifyOnError = true,
+    })
+    local Tabs = {
+        Key = Window:AddKeyTab("Key System"),
+        Guide = Window:AddTab("Guide", "heart-handshake"),
+    }
+    Tabs.Guide:UpdateWarningBox({
+        Visible = true,
+        Title = "Rift",
+        Text = "Rift can be keyless — enjoy access for just $3.75 via Crypto. Buy now at shoppy.gg/@SynergyNetworks."
+    })
+    local GuideSection = Tabs.Guide:AddLeftGroupbox("Guide")
+    local Games = Tabs.Guide:AddLeftGroupbox("Games Supported")
+    local FAQSection = Tabs.Guide:AddRightGroupbox("FAQ / Issues")
+    Tabs.Key:AddLabel({
+        Text = "To copy the key link, go to the Guide section.\nOnce you've got your key, paste it into the box below and click Execute",
+        DoesWrap = true,
+        Size = 16,
+    })
+    Tabs.Key:AddKeyBox(function(_, ReceivedKey)
+        if Libraries.API.check_key(ReceivedKey).code == "KEY_VALID" then
+            getfenv().script_key = ReceivedKey
+            writefile("RiftAssets/SavedKey.txt", ReceivedKey)
+            KeyValidated = true
+        else
+            Libraries.Library:Notify({
+                Title = string.format('<font color="rgb(%d, %d, %d)">◆ Invalid key</font>', 
+                    Libraries.Library.Scheme.AccentColor.R * 255, 
+                    Libraries.Library.Scheme.AccentColor.G * 255, 
+                    Libraries.Library.Scheme.AccentColor.B * 255
+                ),
+                Description = "This key is not valid. Please try again.",
+                Time = 5,
+            }) 
+        end
+    end)
+    local GuideSteps = {
+        "Click the button below to get your key link.",
+        "Paste the link into your browser to access the key generation page.",
+        "Complete the Lootlabs offers by selecting tasks and waiting for the required time.",
+        "If popups appear, close them to continue.",
+        "Return to the Key System and submit your generated key."
+    }
+    for Index, Step in next, GuideSteps do
+        GuideSection:AddLabel(string.format('<b><font color="rgb(%d, %d, %d)">%d)</font></b> %s',
+            Libraries.Library.Scheme.AccentColor.R * 255,
+            Libraries.Library.Scheme.AccentColor.G * 255,
+            Libraries.Library.Scheme.AccentColor.B * 255,
+            Index, Step
+        ), true)
+    end
+    local GamesSupported = {
+        [1] = "Fisch",
+        [2] = "Dead Rails",
+        [3] = "Forsaken"
+    }
+    for Index, Step in next, GamesSupported do
+        Games:AddLabel(string.format('<b><font color="rgb(%d, %d, %d)">%d)</font></b> %s',
+            Libraries.Library.Scheme.AccentColor.R * 255,
+            Libraries.Library.Scheme.AccentColor.G * 255,
+            Libraries.Library.Scheme.AccentColor.B * 255,
+            Index, Step
+        ), true)
+    end
+    GuideSection:AddButton("Copy Key Link", function()
+        setclipboard("https://ads.luarmor.net/get_key?for=Rift-DvnVpWgEvHMb")
+        Libraries.Library:Notify({
+            Title = string.format('<font color="rgb(%d, %d, %d)">◆ Copied to clipboard</font>', 
+                Libraries.Library.Scheme.AccentColor.R * 255, 
+                Libraries.Library.Scheme.AccentColor.G * 255, 
+                Libraries.Library.Scheme.AccentColor.B * 255
+            ),
+            Description = "Paste the key link into your browser of choice.",
+            Time = 5,
+        }) 
+    end)
+    local FAQEntries = {
+        {
+            "◆ The page never loads!",
+            {
+                "Switch to a browser like Google Chrome.",
+                "Disable firewalls, VPN/proxy services, or antiviruses."
+            }
+        },
+        {
+            "◆ The tasks are never complete.",
+            {
+                "Disable any ad blockers such as UBlock Origin.",
+                "Refresh the page and try again."
+            }
+        },
+        {
+            "◆ My key is invalid.",
+            {
+                "Ensure that there are no whitespaces in the key inputted.",
+                "Keys generated by other people cannot be used by you!"
+            }
+        },
+        {
+            "◆ Lifetime Keys",
+            {
+                "These do not exist at the moment. However, it may be implemented in the future."
+            }
+        }
+    }
+    for _, Entry in next, FAQEntries do
+        FAQSection:AddLabel(string.format('<font color="rgb(%d, %d, %d)">%s</font>',
+            Libraries.Library.Scheme.AccentColor.R * 255,
+            Libraries.Library.Scheme.AccentColor.G * 255,
+            Libraries.Library.Scheme.AccentColor.B * 255,
+            Entry[1]
+        ), true)
+        for Index, Step in next, Entry[2] do
+            FAQSection:AddLabel(string.format('<b><font color="rgb(%d, %d, %d)">%d)</font></b> %s',
+                Libraries.Library.Scheme.AccentColor.R * 255,
+                Libraries.Library.Scheme.AccentColor.G * 255,
+                Libraries.Library.Scheme.AccentColor.B * 255,
+                Index, Step
+            ), true)
+        end
+    end
+
+    Libraries.Library:Notify({
+        Title = string.format('<font color="rgb(%d, %d, %d)">◆ A new game is supported</font>', 
+            Libraries.Library.Scheme.AccentColor.R * 255, 
+            Libraries.Library.Scheme.AccentColor.G * 255, 
+            Libraries.Library.Scheme.AccentColor.B * 255
+        ),
+        Description = "Dead Rails has been added to Rift along with the current games. Execute Rift in the game itself and it will load.",
+        Time = 5,
+    }) 
+end
+
+repeat task.wait() until KeyValidated
+
+if getgenv().Library then
+    getgenv().Library:Unload()
+    getgenv().Library = nil
+end
+
+getgenv().script_key = getfenv().script_key
+Libraries.API.load_script()
+getgenv().script_key = nil
