f0464cb4398948f33b92356ba1721918daa5ee13
code-bytee <bekketristan@gmail.com>
2025-04-26 11:09:26 +0200

    Add files via upload

---
+++ Assets/Information.lua
@@ -0,0 +1,16 @@
+return {
+    VERSION = "1.25.6.8",
+    CHANNEL = "Beta",
+    ADVERTISEMENT_ENABLED = true,
+    CHANGELOGS = "<b><u>All Games</u></b>\n• Added a 'Home' tab\n• Migrated to new library version\n• Advertisements will no longer show if you have paid for a key",
+    GAMES_SUPPORTED =   "<font color=\"#00ff00\">◉</font> → Maintained & Updated\n" ..
+                        "<font color=\"#ffff00\">◉</font> → Experimental / Has Issues\n" ..
+                        "<font color=\"#ff0000\">◉</font> → Offline / Broken\n\n" ..
+                        "• <b>Dead Rails</b> <font color=\"#00ff00\">◉</font>\n" ..
+                        "• <b>Fisch</b> <font color=\"#00ff00\">◉</font>\n" ..
+                        "• <b>Forsaken</b> <font color=\"#00ff00\">◉</font>\n\n" ..
+                        "Executing Rift outside of these games will not work.",
+    ADVERTISEMENTS = {
+        "Rift can be keyless — enjoy access for just $3.75 via Crypto. Buy now at https://rifton.top"
+    }
+}
